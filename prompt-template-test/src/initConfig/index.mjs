f6dcd7c881a09e7d8ba4dafd6822a6506acf1959
judy.chen <judy.chen@temu.com>
2026-04-21 19:56:29 +0800

    feat: create

--- prompt-template-test/src/initConfig/index.mjs
+++ prompt-template-test/src/initConfig/index.mjs
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { ChatOpenAI } from '@langchain/openai';
+import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 dotenv.config({ path: join(__dirname, '../../../.env'), quiet: true });
+
+const VECTOR_DIM = 1024;
 
 const model = new ChatOpenAI({
     modelName: process.env.MODEL_NAME,
@@ -14,6 +16,18 @@ const model = new ChatOpenAI({
     }
 });
 
+const embedding = new OpenAIEmbeddings({
+    modelName: process.env.EMBEDDINGS_MODEL_NAME,
+    apiKey: process.env.API_KEY,
+    configuration: {
+        baseURL: process.env.BASE_URL
+    },
+    dimensions: VECTOR_DIM,
+})
+
+
 export {
-    model
+    model,
+    embedding,
+    VECTOR_DIM
 }
