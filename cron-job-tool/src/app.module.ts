e01ab6e6fd44a0710c9cebc5d07e923743ec6f4f
judy.chen <judy.chen@temu.com>
2026-05-10 15:25:56 +0800

    feat: db user

--- cron-job-tool/src/app.module.ts
+++ cron-job-tool/src/app.module.ts
@@ -6,6 +6,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { join } from 'path';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
@@ -35,6 +37,18 @@ import { ServeStaticModule } from '@nestjs/serve-static';
           },
         };
       },
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'admin',
+      database: 'hello',
+      synchronize: true,
+      connectorPackage: 'mysql2',
+      logging: true,
+      entities: [User],
     }),
   ],
   controllers: [AppController],
---
+++ cron-job-tool/src/users/entities/user.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    length: 50,
+  })
+  name: string;
+
+  @Column({
+    length: 50,
+  })
+  email: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+  })
+  updatedAt: Date;
+}
