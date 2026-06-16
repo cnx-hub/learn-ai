15070ddd4870ef9ee4139b1a8f3093ef83e39788
judy.chen <judy.chen@temu.com>
2026-06-17 00:02:56 +0800

    feat: bookServeice

--- nest-dockerfile-test/src/app.module.ts
+++ nest-dockerfile-test/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Book } from './book/entities/book.entity';
 
 @Module({
-  imports: [BookModule],
+  imports: [
+    BookModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'admin',
+      database: 'book',
+      synchronize: true,
+      logging: true,
+      entities: [Book],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
