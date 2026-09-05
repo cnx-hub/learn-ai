3d65d41e03853953ff63b7be8b2bed79695e6b47
judy.chen <judy.chen@temu.com>
2026-09-06 06:53:31 +0800

    feat: curl

--- nest-feature/src/user/user.module.ts
+++ nest-feature/src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { AuthGuard } from '../common/guards/auth.guard';
+
 import { UserController } from './user.controller';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, AuthGuard],
 })
 export class UserModule {}
